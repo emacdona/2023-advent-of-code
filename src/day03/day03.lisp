2c83c2d23e5ec5b5dcdc65ec4f35b52d6a4a115d
Ed <edmacdonald555@gmail.com>
2023-12-21 19:24:44 +0000

    Adding objects and pretty-printing of them

--- src/day03/day03.lisp
+++ src/day03/day03.lisp
@@ -21,28 +21,75 @@
       for col on (car row)
       for j = 0 then (+ 1 j)
       collect
-      (list
-       ;; top boundary?
-       (if (= i 0) t nil)
+      (make-instance
+       'token
+       :token-char (car col)
+       :i i
+       :j j
+       :top-boundary-p (if (= i 0) t nil)
+       :bottom-boundary-p (if (cdr row) nil t)
+       :left-boundary-p (if (= j 0) t nil)
+       :right-boundary-p (if (cdr col) nil t)
+       ))))
 
-       ;; left boundary?
-       (if (= j 0) t nil)
+(defclass token ()
+  ((char
+    :initarg :token-char
+    :reader token-char)
+   (i
+    :initarg :i
+    :reader i)
+   (j
+    :initarg :j
+    :reader j)
+   (top-boundary-p
+    :initarg :top-boundary-p
+    :reader top-boundary-p)
+   (bottom-boundary-p
+    :initarg :bottom-boundary-p
+    :reader bottom-boundary-p)
+   (left-boundary-p
+    :initarg :left-boundary-p
+    :reader left-boundary-p)
+   (right-boundary-p
+    :initarg :right-boundary-p
+    :reader right-boundary-p)
+   ))
 
-       ;; bottom boundary
-       (if (cdr row) nil t)
+(defmethod print-object ((obj token) out)
+  (print-unreadable-object (obj out :type t)
+    (format
+     out "(~d,~d): '~a' ~a"
+     (i obj)
+     (j obj)
+     (token-char obj)
+     (cond
+       ((and
+        (top-boundary-p obj)
+        (left-boundary-p obj))
+        #\NORTH_WEST_TRIANGLE-HEADED_ARROW_TO_BAR)
 
+       ((and
+         (top-boundary-p obj)
+         (right-boundary-p obj))
+        #\NORTH_EAST_TRIANGLE-HEADED_ARROW_TO_BAR)
 
-       ;; right boundary
-       (if (cdr col) nil t)
+       ((and
+         (bottom-boundary-p obj)
+         (left-boundary-p obj))
+        #\SOUTH_WEST_TRIANGLE-HEADED_ARROW_TO_BAR)
 
-       i
-       j
-       (car col)))))
+       ((and
+        (bottom-boundary-p obj)
+        (right-boundary-p obj))
+        #\SOUTH_EAST_TRIANGLE-HEADED_ARROW_TO_BAR)
 
-(defclass number-region ()
-  (digits
-   top-left-coord
-   lower-right-coord))
+       ((top-boundary-p obj) #\UPWARDS_TRIANGLE-HEADED_ARROW_TO_BAR)
 
-(defun find-symbols (grid)
-  )
+       ((bottom-boundary-p obj) #\DOWNWARDS_TRIANGLE-HEADED_ARROW_TO_BAR)
+
+       ((left-boundary-p obj) #\LEFTWARDS_TRIANGLE-HEADED_ARROW_TO_BAR)
+
+       ((right-boundary-p obj) #\RIGHTWARDS_TRIANGLE-HEADED_ARROW_TO_BAR)
+
+       (t " ")))))
